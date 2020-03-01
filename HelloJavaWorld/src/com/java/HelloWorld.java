cd1ae0d474ce2f436c98dd2bc0ef4c89df13beda
punitharid1 <punitharid@rediffmail.com>
2020-03-01 14:41:06 +0530

    //This is a by new developer1

--- HelloJavaWorld/src/com/java/HelloWorld.java
+++ HelloJavaWorld/src/com/java/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 public static void main(String args[])
 {
 
-	//This is a by new developer
+	//This is a by new developer1
 	System.out.println("Hello World");	
 Test testObj = new Test();
 testObj.method1();
