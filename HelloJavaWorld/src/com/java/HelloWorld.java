6a22e833e7636e9c398abf1a0c22ec8a35611d28
punitharid <punitharid@rediffmail.com>
2020-03-01 14:15:30 +0530

    new developer commit

--- HelloJavaWorld/src/com/java/HelloWorld.java
+++ HelloJavaWorld/src/com/java/HelloWorld.java
@@ -3,7 +3,9 @@ package com.java;
 public class HelloWorld {
 public static void main(String args[])
 {
-System.out.println("Hello World");	
+
+	//This is a by new developer
+	System.out.println("Hello World");	
 Test testObj = new Test();
 testObj.method1();
 }
