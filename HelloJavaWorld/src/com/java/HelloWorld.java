6e73368929455d5224c65a007cfcc328b883afdf
punitha <punitharid@rediffmail.com>
2020-03-09 11:56:08 +0530

    Checking major and minor devops

--- HelloJavaWorld/src/com/java/HelloWorld.java
+++ HelloJavaWorld/src/com/java/HelloWorld.java
@@ -6,7 +6,13 @@ public static void main(String args[])
 
 	//This is a by new developer1
 	System.out.println("Hello World");	
-Test testObj = new Test();
-testObj.method1();
+	Test testObj = new Test();
+	testObj.method1();
+	int count=0;
+	if (count <1)
+	  {
+		System.out.println("Test major and minor errors");
+		
+	  }
 }
 }
